38ce912c098433e4e4dc48d4ad18e928446ce127
Grant Bentley <gbentley0618@comcast.net>
2023-11-16 15:21:17 -0600

    Work on complete incomplete  toggle functinality

--- src/component/ToDoTask.jsx
+++ src/component/ToDoTask.jsx
@@ -3,15 +3,29 @@ import ToDoContext from '../context/ToDoContext'
 import { useContext, useEffect } from 'react'
 
 function ToDoTask() {
-
+  
   const {ToDos} = useContext(ToDoContext)
+  
+  let status = "Task Incomplete"
+  const showStatusStyling = (status) => {
+    
+  // let status = "Task Incomplete"
+  //   if (ToDos.list[3] === false) {
+      // let status = "Task Incomplete"
+  //   }
+
+
+  
+  }
+
+  
  
   return (
     
     <div >
       <ul className='Task-List'>
         {ToDos.map((Todo, index) => (
-        <li className="Task Complete" key={index}>{Todo.title}</li>
+        <li className={status} key={index}>{Todo.title}</li>
         ))}
       </ul>
     </div>
