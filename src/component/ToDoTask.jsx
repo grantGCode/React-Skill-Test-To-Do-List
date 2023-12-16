de9d6238fe3646481874cd689163f606579511d0
Grant Bentley <gbentley0618@comcast.net>
2023-12-16 01:20:10 -0600

    save progress

--- src/component/ToDoTask.jsx
+++ src/component/ToDoTask.jsx
@@ -5,13 +5,40 @@ import { useContext, useEffect } from 'react'
 function ToDoTask() {
 
   const {ToDos} = useContext(ToDoContext)
+
+  
+    
+  const noStatus = "Task StatusUnkonwn"
+  const notComplete = "Task Incomplete"
+  const Complete =  "Task Complete"
+  let status = noStatus
+
+  ToDos.forEach((element) => {
+    if (element.Completed === false) {
+      let status = notComplete
+      console.log(element)
+    } else if (element.Completed === true) {
+      let status = Complete
+      console.log(element)
+    }
+    
+     
+  })
+
+  
+
+
+
+  
+  
+  
  
   return (
     
     <div >
       <ul className='Task-List'>
         {ToDos.map((Todo, index) => (
-        <li className="Task Complete" key={index}>{Todo.title}</li>
+        <li className={status} key={index}>{Todo.title}</li>
         ))}
       </ul>
     </div>
